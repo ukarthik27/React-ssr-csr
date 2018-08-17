88e7ca03ad8aef3a0e46bc98f8e3bd8c7daf2234
karthik_u <karthikudhayakumar.u@caratlane.com>
2018-08-17 10:25:32 +0530

    login with data return

--- src/client/Home.js
+++ src/client/Home.js
@@ -29,7 +29,9 @@ class Home extends React.Component {
     }
     render() {
         console.log("HOME - this.props.items", this.props.items)
-        console.log("History : ", history);
+        console.log("History : ", history, this.props.items.isLoggedIn);
+        console.log("---- Home isLoggeIn :", this.props.items.isLoggedIn)
+
         var pageItems = this.props.items.data
         const pageData = pageItems.map((item) => {
             return <div className="home-data-div" key={Math.random()}>
@@ -37,11 +39,24 @@ class Home extends React.Component {
                 <div className="home-data-div-data">{item.data}</div>
             </div>
         })
+        var userdataDiv;
+        if (this.props.items.isLoggedIn) {
+            console.log("--client Home : userData ", this.props.items.userData)
+            if (this.props.items.userData.userdata !== "undefined") {
+                userdataDiv = this.props.items.userData.userData.map((item) => {
+                    return <div className="home-data-div" key={Math.random()}>
+                        <div className="home-data-div-heading">{item.title}</div>
+                        <div className="home-data-div-data">{item.data}</div>
+                    </div>
+                })
+            }
+        }
         //console.log(pageData)
         return (
             <React.Fragment>
                 <div id="home-heading">Home</div>
                 {pageData}
+                {userdataDiv}
             </React.Fragment>
         )
     }
